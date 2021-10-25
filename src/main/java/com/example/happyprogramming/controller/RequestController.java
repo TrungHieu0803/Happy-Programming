627618527b192d5d9c0d9b0eed87f97e4f9db6d3
Phạm Thị Vân Anh _ HE150038 <61355176+VanAnhHE150038@users.noreply.github.com>
2021-10-25 12:08:22 +0700

    update request controller

--- src/main/java/com/example/happyprogramming/controller/RequestController.java
+++ src/main/java/com/example/happyprogramming/controller/RequestController.java
@@ -1,12 +1,15 @@
 package com.example.happyprogramming.controller;
 
 
+import com.example.happyprogramming.Entity.Pagination;
 import com.example.happyprogramming.Entity.RequestEntity;
 import com.example.happyprogramming.Entity.SkillEntity;
 import com.example.happyprogramming.Entity.UserEntity;
 import com.example.happyprogramming.repository.RequestRepository;
+import com.example.happyprogramming.repository.UserRepository;
 import com.example.happyprogramming.service.RequestService;
 import com.example.happyprogramming.service.SkillService;
+import com.example.happyprogramming.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +17,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -36,22 +41,40 @@ public class RequestController {
     @Autowired
     RequestRepository requestRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/create-request")
     public String createRequestPage(Model model){
         ArrayList<SkillEntity> listSkill = skillService.getAllSkill();
         model.addAttribute("listSkill",listSkill);
         model.addAttribute("requestForm",new RequestEntity());
-        return "/client/create-request";
+        model.addAttribute("recommend",true);
+        return "client/create-request";
     }
 
     @PostMapping("/create-request")
-    public String createRequest(@ModelAttribute("requestForm") RequestEntity requestEntity) {
+    public String createRequest(@ModelAttribute("requestForm") RequestEntity requestEntity,
+                                @RequestParam("recommend") boolean recommend,HttpServletRequest request) {
         UserEntity user =(UserEntity) session.getAttribute("userInformation");
-        requestEntity.setMenteeId(user);
-        java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-        requestEntity.setCreatedDate(date);
-        requestService.createRequest(requestEntity);
-        return "redirect:/home";
+        if(recommend){
+
+            requestEntity.setMenteeId(user);
+            java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+            requestEntity.setCreatedDate(date);
+            requestService.createRequest(requestEntity);
+            return "redirect:/home";
+        }else{
+            int mentorId= Integer.parseInt(request.getParameter("mentorId"));
+            UserEntity userEntity = userRepository.findById(mentorId);
+            requestEntity.setMentorId(userEntity);
+            requestEntity.setMentorName(userEntity.getFullName());
+            requestEntity.setMenteeId(user);
+            java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+            requestEntity.setCreatedDate(date);
+            requestService.createRequest(requestEntity);
+            return "redirect:/home";
+        }
     }
 
     @GetMapping("/invited-request-wait")
@@ -73,19 +96,19 @@ public class RequestController {
         UserEntity user =(UserEntity) session.getAttribute("userInformation");
         List<RequestEntity> list = requestService.findRequestEntitiesByMentorIdAndStatus(user, 2);
         model.addAttribute("listRejectedRequest",list);
-        return "/client/rejected-requests";
+        return "client/rejected-requests";
     }
     @GetMapping("/invited-request-approved")
     public String listApprovedRequestPage(Model model){
         UserEntity user =(UserEntity) session.getAttribute("userInformation");
         List<RequestEntity> list = requestService.findRequestEntitiesByMentorIdAndStatus(user, 3);
         model.addAttribute("listApprovedRequest",list);
-        return "/client/approved-requests";
+        return "client/approved-requests";
     }
     @PostMapping("/reject")
     public String rejectRequest(HttpServletRequest request){
-       Long id = Long.parseLong(request.getParameter("id"));
-       String response = request.getParameter("response");
+        Long id = Long.parseLong(request.getParameter("id"));
+        String response = request.getParameter("response");
         Optional<RequestEntity> re = requestService.findById(id);
         if (re.isPresent()){
             RequestEntity req = re.get();
@@ -109,10 +132,29 @@ public class RequestController {
         return "redirect:/invited-request-wait";
     }
     @GetMapping("/list-requests")
-    public String listRequest(Model model, @RequestParam ("status") int status){
-        ArrayList<RequestEntity> listRequest = requestService.findByStatus(status);
-        model.addAttribute("listRequests", listRequest);
-        return "/client/list-requests";
+    public String listRequest(Model model, @RequestParam ("status") int status,
+                              @RequestParam(value = "pageNumber",required = false,defaultValue = "1")int pageNumber){
+        Pagination<RequestEntity> listRequest = requestService.findByStatus(status,pageNumber);
+        model.addAttribute("listRequests", listRequest.getPaginatedList());
+        model.addAttribute("pageNumbers",listRequest.getPageNumbers());
+        model.addAttribute("status",status);
+        model.addAttribute("currentPage",pageNumber);
+        return "client/list-requests";
     }
+    @GetMapping("/update-request")
+    public  String updateRequest(@RequestParam("id") long id, Model model){
+        RequestEntity request = requestRepository.getById(id);
+        model.addAttribute("request", request);
+        model.addAttribute("requestForm",new RequestEntity());
+        return "client/update-request";
+    }
+    @PostMapping("/update-request")
+    public  String doUpdate(@ModelAttribute("requestForm") RequestEntity requestEntity, HttpServletRequest request){
+        long requestId = Integer.parseInt(request.getParameter("requestId"));
+        requestEntity.setId(requestId);
+        requestService.updateRequest(requestEntity);
+        return "redirect:/list-requests?status=0";
+    }
+
 
 }
